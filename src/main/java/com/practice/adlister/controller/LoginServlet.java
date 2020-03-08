b73ff5f9d845d5df3ff6165e64b077b6ef25c06a
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-03-08 01:04:29 -0600

    updated

---
+++ src/main/java/com/practice/adlister/controller/LoginServlet.java
@@ -0,0 +1,8 @@
+package com.practice.adlister.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet(name ="controllers.LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+
+}
