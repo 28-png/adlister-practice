63dbb0a5a5e575aa51055ec67e023e17c8759407
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-03-08 18:45:30 -0500

    updated

--- src/main/java/com/practice/adlister/controller/LoginServlet.java
+++ src/main/java/com/practice/adlister/controller/LoginServlet.java
@@ -1,8 +1,20 @@
 package com.practice.adlister.controller;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @WebServlet(name ="controllers.LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if(request.getSession().getAttribute("user") != null) {
+            response.sendRedirect("/profile");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+    }
 }
+
